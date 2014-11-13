510e6e5f2ff9d580a9fb4c65a1ff5eef0f20aca9
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-11-13 16:04:34 +0530

    forms

--- app/controllers/addController.js
+++ app/controllers/addController.js
@@ -1,4 +1,26 @@
 angular.module('myApp').controller('addController', function($scope, hotelsData) {
+	var defaultHotel = {
+		price : 5000,
+		amenities : [{
+			name : 'Gym'
+		},{
+			name : 'Wifi'
+		}
+		]
+	};
+
+	$scope.addAmenity = function(newAmenity){
+		$scope.newHotel.amenities.push({
+			name : newAmenity
+		})
+	}
+
+	$scope.newHotel = angular.copy(defaultHotel);
+
+	$scope.reset = function(){
+		$scope.newHotel = angular.copy(defaultHotel);
+	}
+
 	$scope.addHotel = function() {
 
 		hotelsData.addHotel($scope.newHotel);
